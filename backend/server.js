37374cdbe4cebf77183c88a9c5bd9f609eb2aafa
AKASH KUMAR <akashkumarcs23@gmail.com>
2024-12-25 19:03:45 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,10 +10,8 @@ import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
 
 import connectDB from "./db/connectDB.js";
-import path from "path";
 
-const app = express();
-dotenv.config(); // to access .env variables
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -20,12 +19,15 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(express.json({ limit: "5mb" })); // to parse/use req.body
-app.use(express.urlencoded({ extended: true })); // to parse form data in urlencoded through postman
-app.use(cookieParser()); // to get cookie from client to server for validating user
-
+const app = express();
 const PORT = process.env.PORT || 5000;
-const _dirname = path.resolve();
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -41,6 +43,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-  console.log(`Server is running at PORT: ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
